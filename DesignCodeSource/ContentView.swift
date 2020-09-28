156ed398363e67619d93ee74b3fd1e9c5b253058
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-09-28 23:49:45 +0700

    *Lesson 2 - SwiftUI Visual Editor: Create basic UI

--- DesignCodeSource/ContentView.swift
+++ DesignCodeSource/ContentView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                HStack {
+                    Spacer()
+                    Image("Illustration 1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                Spacer()
+            }
+            Text("SwiftUI IOS14")
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+            Text("20 sections")
+                .font(.footnote)
+                .foregroundColor(Color.white)
+        }
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .previewLayout(.fixed(width: 200.0, height: 200.0))
+                .preferredColorScheme(.dark)
+        }
     }
 }
