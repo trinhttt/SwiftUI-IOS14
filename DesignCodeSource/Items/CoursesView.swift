7461e08a20060f5b5f7c62ec36324d62bcc1513d
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-10-03 09:25:24 +0700

    Lesson 7: Show view and transitions

--- DesignCodeSource/Items/CoursesView.swift
+++ DesignCodeSource/Items/CoursesView.swift
@@ -9,8 +9,25 @@
 import SwiftUI
 
 struct CoursesView: View {
+    @State var show = false
     var body: some View {
-        Text("")
+        ZStack {
+            CourseItem()
+                .frame(width: 335, height: 250)
+            VStack {
+                if show {
+                    CourseItem()
+                        .transition(.move(edge: .bottom))
+                        .edgesIgnoringSafeArea(.all)
+                }
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                show.toggle()
+            }
+        }
+//        .animation(.easeInOut)
     }
 }
 
