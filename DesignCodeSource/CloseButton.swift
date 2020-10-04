3388a6d6e81e7e6e71d804d15b9eed197e043f68
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-10-04 16:17:28 +0700

    Lesson 12 + 13:  Lazy Grid Layout & Matched Container and zIndex

---
+++ DesignCodeSource/CloseButton.swift
@@ -0,0 +1,26 @@
+//
+//  CloseButton.swift
+//  DesignCodeSource
+//
+//  Created by Trinh Thai on 10/4/20.
+//  Copyright © 2020 Trinh Thai. All rights reserved.
+//
+
+import SwiftUI
+
+struct CloseButton: View {
+    var body: some View {
+        Image(systemName: "xmark")
+            .font(.system(size: 17, weight: .bold))
+            .foregroundColor(.white)
+            .padding(.all, 10)
+            .background(Color.black.opacity(0.6))
+            .clipShape(Circle())
+    }
+}
+
+struct CloseButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CloseButton()
+    }
+}
